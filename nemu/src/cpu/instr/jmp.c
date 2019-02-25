6b5f6277bf64fd4154098d3c0e65391b8c4fee0e
ICS PA <liang.wang.84@gmail.com>
2019-02-25 14:46:16 +0800

    fix instr length in print_asm

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -10,7 +10,7 @@ make_instr_func(jmp_near) {
         operand_read(&rel);
 
 	int offset = sign_ext(rel.val, data_size);
-	print_asm_1("jmp", "", 2, &rel);
+	print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
 	cpu.eip += offset;
 
